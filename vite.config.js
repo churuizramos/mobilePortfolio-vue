87a5772bc4c2a93c9d3dca829624179a84d25bd9
Jesus <ruizramos04@outlook.com>
2024-09-01 16:05:47 -0400

    added vite config reference to repo

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mobilePortfolio-vue",
   plugins: [
     vue(),
     vueDevTools(),
