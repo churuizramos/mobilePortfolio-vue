3874a31fb79ba2c7ecf9a7048bf8199e653437c6
Jesus <ruizramos04@outlook.com>
2024-09-01 23:37:05 -0400

    header v1 finished

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,30 @@
 </script>
 
 <template>
-  $END$
+  <div class="font-archivo-black flex flex-col text-center gap-5">
+    <h1 class="text-6xl my-12">JESUS RUIZ RAMOS</h1>
+    <div class="flex flex-row h-max-1/4 m-auto">
+      <img src="/icons8-csharp.svg" alt="C sharp icon"/>
+      <img src="/icons8-c.svg" alt="C icon"/>
+      <img src="/icons8-cpp.svg" alt="C plus plus icon"/>
+      <img src="/icons8-java.svg" alt="Java icon"/>
+      <img src="/icons8-html.svg" alt="HTML icon"/>
+      <img src="/icons8-css.svg" alt="CSS icon"/>
+    </div>
+
+    <div class="flex flex-col gap-3 bg-c-emerald-3 rounded-2xl mx-6 p-4">
+      <h1 class="text-c-orange-1 text-2xl">B.S. COMPUTER SCIENCE AND ENGINEERING</h1>
+      <a href="https://engineering.louisville.edu/" target="_blank">
+        <div class="flex flex-row bg-gray-300 text-black font-archivo-narrow p-2 rounded-2xl">
+          <img class="w-20" src="/louisville_cardinals_football.png" height="328" width="492"/>
+          <div class="flex flex-col">
+            <p>J.B. Speed School of Engineering</p>
+            <p>University of Louisville</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
 
 <template>
   <nav class="flex justify-between items-center w-[92%] p-3 font-archivo-black">
-    <img class="w-16 rounded-4xl" src="/src/images/pic_self_pfp.jpg" alt="My profile picture. (1920x1920)"/>
+    <img class="w-16 rounded-4xl" src="/pic_self_pfp.jpg" alt="My profile picture. (1920x1920)"/>
     <h1 class="text-3xl">churuizramos</h1>
     <a class="bg-blue-600 text-gray-300 font-archivo-regular text-2 px-3 rounded-full" href="https://s3.amazonaws.com/symp.csm.usprod/cardinalcareers/files/800/800b10f34a844fa20f838a854febe8af.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAID3RBESXBCESHUGA%2F20240902%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240902T024019Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=1079a9586725d5cd5d703381a9cdeda116d5581388ef6421781deea0308fe682" target="_blank"><p>Resume</p></a>
   </nav>
